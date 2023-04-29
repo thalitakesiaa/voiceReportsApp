d89c0dca9555dca81129e4828c30e0379605ccf0
thalitakesia <thalitakesia@academico.ufs.br>
2023-04-28 22:24:03 -0300

    feat: Login e Integração com api

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import Signin from './src/pages/signin'
 import Home from './src/pages/home';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
+  
+    const Stack = createStackNavigator();
 
-  const Stack = createStackNavigator();
-
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Signin" component={Signin} />
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Signin" component={Signin} />
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
 }
 
